243b3d84d8b525737304d2b66697119e82cf7bc6
dnhthsn <dinhthaison2000@gmail.com>
2023-06-05 09:19:41 +0700

    Initial Commit

---
+++ ArtSell/ArtSellApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtSellApp.swift
+//  ArtSell
+//
+//  Created by Đinh Thái Sơn on 05/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ArtSellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
